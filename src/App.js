11475d3a6ed7e934417cb080f726fb98bc011da4
aidanasm <smanova707@gmail.com>
2022-07-14 16:36:57 +0600

    added some logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 
 function App() {
-  function handleClick() {}
+  const [counter, setCounter] = useState(0)
+
+  const handlePlus =()=>{
+    setCounter(counter+1)
+  }
+  const handleMinus =()=>{
+    setCounter(counter-1)
+  }
   return (
     <div>
       <h1>Git progect</h1>
-      <div>0</div>
-      <Button handleClick={handleClick}>-</Button>
-      <Button handleClick={handleClick}>+</Button>
+      <div>{counter}</div>
+      <Button handleClick={handleMinus}>-</Button>
+      <Button handleClick={handlePlus}>+</Button>
     </div>
   );
 }
